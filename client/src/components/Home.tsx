812bf01e7132d6edb44a957c746c9240acc7e5b3
alex olivares <alexander.b.olivares@gmail.com>
2021-06-21 21:35:08 -0500

    mui installed. skeleton of nav/home components

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import {
+  Button,
+  Container,
+  TextField,
+  makeStyles,
+  Theme,
+  createStyles,
+  Typography,
+} from "@material-ui/core";
+
+const useStyles = makeStyles((theme: Theme) => {
+  return createStyles({
+    root: {
+      "& > *": {
+        margin: theme.spacing(1),
+        width: "25ch",
+      },
+    },
+    div: {
+      padding: "20%",
+    },
+  });
+});
 
 const Home = () => {
-	return (
-		<div>
-			<h1>Travis Heights Wiffle Ball League</h1>	
-		</div>
-	)
-}
+  const classes = useStyles();
+  return (
+    <Container maxWidth="md">
+      <div>
+        <Typography variant="h3">Travis Heights Wiffle Ball League</Typography>
+      </div>
+      <div className={classes.div}>pictures</div>
+      <div className={classes.div}>map</div>
+      <div>
+        <form className={classes.root} noValidate autoComplete="off">
+          <TextField id="outlined-basic" label="Name" variant="outlined" />
+          <TextField id="outlined-basic" label="Email" variant="outlined" />
+          <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </form>
+      </div>
+      <div>Join for free!</div>
+      <Button variant="contained" color="primary">
+        Sign Up
+      </Button>
+    </Container>
+  );
+};
 
 export default Home;
--- client/src/components/Navbar.tsx
+++ client/src/components/Navbar.tsx
@@ -1,20 +1,48 @@
-import React from 'react'
+import React from "react";
 import { Link } from "react-router-dom";
+import { createStyles, makeStyles, Theme } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import IconButton from "@material-ui/core/IconButton";
+import MenuIcon from "@material-ui/icons/Menu";
 
-const Navbar: React.FC = () => {
-	return (
-		<div style={{display: 'flex', justifyContent: "space-around"}}>
-			<div>
-			<Link to="/">Home</Link>	
-				</div>	
-			<div>
-			<Link to="/games">Games</Link>	
-			</div>	
-			<div>
-			<Link to="/contact">Contact</Link>	
-			</div>	
-		</div>
-	)
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+      backgroundColor: "#f2e33b",
+    },
+    menuButton: {
+      marginRight: theme.spacing(2),
+    },
+    title: {
+      flexGrow: 1,
+    },
+  })
+);
+
+function Navbar() {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <AppBar className={classes.root} position="static">
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/">HOME</Link>
+          </Typography>
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/games">GAMES</Link>
+          </Typography>
+          <Typography variant="h6" className={classes.title}>
+            <Link to="/contact">CONTACT</Link>
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </div>
+  );
 }
 
 export default Navbar;
