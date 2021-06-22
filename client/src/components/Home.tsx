373e51d0d0bc112002a962fbc3af5dcece67d3c7
alex olivares <alexander.b.olivares@gmail.com>
2021-06-22 17:48:11 -0500

    slight ui mods on /home

--- client/src/components/Home.tsx
+++ client/src/components/Home.tsx
@@ -7,6 +7,7 @@ import {
   Theme,
   createStyles,
   Typography,
+  Box,
 } from "@material-ui/core";
 
 const useStyles = makeStyles((theme: Theme) => {
@@ -16,9 +17,18 @@ const useStyles = makeStyles((theme: Theme) => {
         margin: theme.spacing(1),
         width: "25ch",
       },
+      "& .MuiTextField-root": {
+        margin: theme.spacing(2),
+        width: "40ch",
+      },
+      textAlign: "center",
+      margin: theme.spacing(2),
     },
     div: {
       padding: "20%",
+    },
+    button: {
+      justify: "center",
     },
   });
 });
@@ -27,22 +37,41 @@ const Home = () => {
   const classes = useStyles();
   return (
     <Container maxWidth="md">
-      <div>
-        <Typography variant="h3">Travis Heights Wiffle Ball League</Typography>
-      </div>
-      <div className={classes.div}>pictures</div>
-      <div className={classes.div}>map</div>
-      <div>
+      <Box m={2}>
+        <Typography align="center" variant="h3">
+          Travis Heights Wiffle Ball League
+        </Typography>
+        <Typography align="center" variant="h6">
+          Free to play and open to all!
+        </Typography>
+      </Box>
+      <Box className={classes.div}>pictures</Box>
+      <Box className={classes.div}>
+        <Typography align="center" variant="h6">
+          Where we play: Little Stacy Park in Travis Heights
+        </Typography>
+        map
+      </Box>
+      <Box>
+        <Typography align="center" variant="h4">
+          Join for free!
+        </Typography>
+        <Typography align="center" variant="h6">
+          We will email you game times each week
+        </Typography>
+      </Box>
+      <Box m="auto">
         <form className={classes.root} noValidate autoComplete="off">
           <TextField id="outlined-basic" label="Name" variant="outlined" />
           <TextField id="outlined-basic" label="Email" variant="outlined" />
           <TextField id="outlined-basic" label="Outlined" variant="outlined" />
         </form>
-      </div>
-      <div>Join for free!</div>
-      <Button variant="contained" color="primary">
-        Sign Up
-      </Button>
+        <Box textAlign="center">
+          <Button variant="contained" color="primary">
+            Sign Up
+          </Button>
+        </Box>
+      </Box>
     </Container>
   );
 };
