00a950682db8f947fadec60843f176f6ccc7eca6
alex olivares <alexander.b.olivares@gmail.com>
2021-08-26 09:15:47 -0500

    catchall route change

--- index.js
+++ index.js
@@ -194,6 +194,10 @@ app.post("/api/admin-login", async (req, res) => {
 // res.redirect("/");
 // });
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "client/build/index.html"));
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
 });
